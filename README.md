5210833e6732416947d9b62c415c313f8fc82cf8
Johnathan Jenkins <john@nixheads.co.uk>
2016-04-16 05:35:03 -0700

    [init] so it begins

--- README.md
+++ README.md
@@ -3,18 +3,23 @@
 Designed for use on the [Linux Lite](https://linuxliteos.com/) Distro. This 
 applet is meant as a quick way to launch the [Linux Lite Control Center](https://github.com/linuxlite/litecontrolcenter).
 
+![](http://i.imgur.com/lCSthWt.png)
+
+## License ![License](https://img.shields.io/badge/license-GPLv2-green.svg)
+
+This project is under the GPLv2 license.
+
 
 ## Depends
 
 - ``libgtk-3-dev``
 - ``pkg-config``
 - ``libxfce4util-dev``
-- ``libexo-1-dev``
 - ``xfce4-dev-tools``
 - ``libxfce4ui-1-dev``
 - ``xfce4-panel-dev``
 - ``libxfconf-0-dev``
 
 ## Author
-
 - [Johnathan "ShaggyTwoDope" Jenkins](https://github.com/shaggytwodope/)
+
---
+++ src/litecontrolcenter.h
@@ -0,0 +1,31 @@
+/*  
+ * Copyright © 2016 Johnathan "Shaggytwodope" Jenkins <twodopeshaggy@gmail.com>
+ *
+ * Distributed under terms of the GPL2 license.
+ */
+
+#ifndef __XFCE_LITECONTROLCENTER_PLUGIN_H__
+#define __XFCE_LITECONTROLCENTER_PLUGIN_H__
+
+G_BEGIN_DECLS
+
+#define DEFAULT_ICON_NAME "lite-controlcenter"
+#define DEFAULT_TOOLTIP_MESSAGE "Linux Lite Control Center"
+#define DEFAULT_RUN_COMMAND "lite-controlcenter"
+
+typedef struct _LitecontrolcenterPlugin
+{
+    XfcePanelPlugin *plugin;
+
+    GtkWidget       *button;
+    GtkWidget       *icon;
+
+    gchar           *icon_name;
+}
+LitecontrolcenterPlugin;
+
+
+G_END_DECLS
+
+
+#endif /* !__XFCE_LITECONTROLCENTER_PLUGIN_H__ */
